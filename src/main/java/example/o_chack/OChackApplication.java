9cd0646ba4d6fa105f6f64964b07adfcdcde7117
winy3ori <winy3ori@gmail.com>
2024-10-28 14:56:39 +0900

    first

---
+++ src/main/java/example/o_chack/OChackApplication.java
@@ -0,0 +1,13 @@
+package example.o_chack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OChackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OChackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/example/o_chack/OChackApplicationTests.java
@@ -0,0 +1,13 @@
+package example.o_chack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OChackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
